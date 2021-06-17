02307b670d21a4f10f21ae7276cbb3e01c7f16ff
Christopher Young <christopher2.h.young@gmail.com>
2021-06-17 01:27:26 -0400

    added an AssetsManager(scene) line for trying to find out if It can give me access to shape keys

--- babylonScene.js
+++ babylonScene.js
@@ -7,14 +7,16 @@ var createScene = function () {
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 		light.intensity = 0.7;
     var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+    var assetsManager = new BABYLON.AssetsManager(scene);
         
 	var url;
 	var fileName;
 		
-        
+        //engine.displayLoadingUI();
         url = 'https://raw.githubusercontent.com/chris45242/BabylonModel/main/';
         fileName = "project.blend1.glb";
           
+        var manager = new BABYLON.MorphTargetManager();
         
 	BABYLON.SceneLoader.ImportMesh("", url, fileName, scene, function (newMeshes) {
 		var mesh = newMeshes[0];
@@ -28,20 +30,32 @@ var createScene = function () {
                 //newMeshes[0].dispose();                
 			mesh.position.copyFromFloats(0, 0, -5);
 			mesh.scaling.copyFromFloats(1,1,1);
-                /*mesh.updateMeshPositions(scramble);
-                var manager = new BABYLON.MorphTargetManager();
+                //mesh.updateMeshPositions(scramble);
+                //var manager = new BABYLON.MorphTargetManager();
+                //var assets = await Scene.loadAsset();
                 mesh.morphTargetManager = manager;
                 
-                var target0 = BABYLON.MorphTarget.FromMesh(mesh, "Casi Smile", 0.25);
-                manager.addTarget(target0);*/
+                //var target0 = BABYLON.MorphTarget.FromMesh(mesh, "Casi Smile", 0.25);
+                //manager.addTarget(target0);
 		//var animations = [];
                 //var morphTarget = mesh.MorphTargetManager.getTarget('Casi Smile');
                 
 		//camera.target = mesh;
 		//console.log(mesh.MorphTargetManager);
-                console.log(mesh);
-                var primitive = scene.getMeshByName("Casi's Body.001_primitive2");
+                console.log(mesh.morphTargetManager);
+                var casiLips = scene.getMeshByName("Casi Body.001_primitive0");
+                var casiInnerMouth = scene.getMeshByName("Casi Body.001_primitive2");
+                var primitive = scene.getMeshByName("Primitives.001");
                 console.log("The mesh's name is: " + primitive);
+                /*var rawData = '';
+                var data = JSON.parse(rawData);
+                var polyCount = 0;
+                for (var meshe of data.meshes)
+                {
+                    polyCount += meshe.indicies.length / 3;
+                    console.log(polyCount);
+                };*/
+                
                 //var assetManager = new BABYLON.AssetsManager();
                 //assetManager.onFinish = function(tasks){
                   //  start();
@@ -51,7 +65,9 @@ var createScene = function () {
 };
 
 var engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true });
+
 var scene = createScene();
+
 
    engine.runRenderLoop(function () {
 	if (scene) {
