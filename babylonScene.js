b4e5a0c9b857941eb257766a97f727a90d0df241
Christopher Young <christopher2.h.young@gmail.com>
2021-06-17 23:58:42 -0400

    made changes to babylonScene.js file

--- babylonScene.js
+++ babylonScene.js
@@ -2,24 +2,30 @@ var canvas = document.getElementById("renderCanvas");
 var createScene = function () {
     var scene = new BABYLON.Scene(engine);
     var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-		camera.setTarget(BABYLON.Vector3.Zero());
 		camera.attachControl(canvas, true);
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 		light.intensity = 0.7;
     var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
     var assetsManager = new BABYLON.AssetsManager(scene);
+    var gui;
         
+        var scales = [];
 	var url;
 	var fileName;
-		
-        //engine.displayLoadingUI();
+        
         url = 'https://raw.githubusercontent.com/chris45242/BabylonModel/main/';
         fileName = "project.blend1.glb";
           
-        var manager = new BABYLON.MorphTargetManager();
+        //var manager = new BABYLON.MorphTargetManager();
         
-	BABYLON.SceneLoader.ImportMesh("", url, fileName, scene, function (newMeshes) {
+	BABYLON.SceneLoader.ImportMesh("", url, fileName, scene, function (newMeshes, particleSystems, skeletons) {
 		var mesh = newMeshes[0];
+                var skeleton = skeletons[0];
+                camera.setTarget = newMeshes[0];
+                var bone = skeleton.bones[0];
+                assetsManager.onFinish = function(tasks){
+                    console.log(scene.meshes.length);
+                };
                 /*var scramble = function(data) {
                     for (index = 0; index < data.length; index ++) {
                         data[index] += 0.1 * Math.random();
@@ -28,8 +34,8 @@ var createScene = function () {
                 //var mesh = newMeshes[0].getChildMeshes()[0];
                 //mesh.setParent(null);
                 //newMeshes[0].dispose();                
-			mesh.position.copyFromFloats(0, 0, -5);
-			mesh.scaling.copyFromFloats(1,1,1);
+		mesh.position.copyFromFloats(0, 0, -5);
+		mesh.scaling.copyFromFloats(1,1,1);
                 //mesh.updateMeshPositions(scramble);
                 //var manager = new BABYLON.MorphTargetManager();
                 //var assets = await Scene.loadAsset();
@@ -60,11 +66,13 @@ var createScene = function () {
                 //assetManager.onFinish = function(tasks){
                   //  start();
                 //};
+                assetsManager.load();
 	});
     return scene;
 };
 
 var engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true });
+
 
 var scene = createScene();
 
